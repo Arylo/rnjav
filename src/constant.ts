fd17f4361d7c4e8e7d0896307a1ff0d4bab45486
doubley_yang <doubley_yang@epam.com>
2022-12-05 17:19:49 +0800

    feat: move constant values to static file

--- src/constant.ts
+++ src/constant.ts
@@ -1,32 +1,22 @@
-export const COMPANY = [
-  'SSIS', 'SQTE', 'JRW', 'MGOLD', 'SORA', 'DVDMS',
-  'CAWD', 'DVAJ', 'PKPL', 'SHKD', 'SSNI', 'STARS',
-  'TIKP', 'UMD', 'SUJI', 'SVDVD', 'IPX', 'HUNTB',
-  'STSK', 'TUS', 'ROOM', 'KAGP', 'REBD', 'SKMJ',
-  'COGM', 'PRED', 'IENF', 'BANK', 'SDAB', 'MDUD',
-  'SCPX', 'JKSR', 'HMN', 'HJMO', 'HALE', 'PFES',
-  'MIDV', 'SGSR', 'SSDV', 'FSDSS', 'CLUB', 'HND',
-  'SDMM', 'MMB', 'ABW', 'REXD', 'MIAA', 'SABA',
-  'APAA', 'HEYZO', 'PKPD', 'BFD', 'ROYD', 'DRPT',
-  'NNPJ', 'SAME', 'YMDS', 'SUPA', 'SDMUA', 'BAB',
-  'BUR', 'CJOD', 'DORI', 'HONB', 'FSAN', 'LLDV',
-  'STCV', 'DOCP', 'BDSR', 'SDNM', 'DANDY', 'AMBI',
-  'MILK', 'NKKD', 'MOGI', 'IGIG', 'HOMA', 'GNAB',
-  'NHDTB', 'HEZ', 'HODV', 'CWP', 'HNDS', 'HOIZ',
-  'CT', 'DLDSS', 'MXX', 'PT', 'MXGS', 'PKPT',
-  'BAHP', 'GS', 'HUNBL', 'IMO', 'JKTU', 'JUKF',
-  'KTRA', 'LOL', 'MDBK', 'MIST', 'SCOP', 'SDDE',
-  'SHIND', 'BAGR', 'PPPE', 'SDMU', 'STP', 'EMOT',
-  'SNTX', 'IPTD', 'XVSR', 'EMAZ', 'T', 'DVRT',
-  'HALT', 'KWBD', 'TPIN', 'SDMF', 'MIFD', 'MFC',
-  'DASD', 'NFD', 'PED', 'CW', 'SNIS', 'ABS',
-  'CHN', 'INST', 'KUN', 'SVVRT', 'HTBET', 'RCT',
-  'HGOT', 'FERA', 'MTALL', 'INSTC', 'NXG', 'RCTD',
-  'FANX', 'SW', 'FTHTD', 'AVOP',
-]
+import fs from 'fs'
+import path from 'path'
+
+// eslint-disable-next-line import/no-mutable-exports
+export let COMPANY: string[] = []
+
+// eslint-disable-next-line import/no-mutable-exports
+export let VIDEO_SUFFIX: string[] = []
+
+try {
+  COMPANY = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../static/company.json'), 'utf-8'))
+} catch (error) {
+}
+
+COMPANY = COMPANY
   .map(a => a.toUpperCase())
   .sort((a, b) => b.length - a.length)
 
-export const VIDEO_SUFFIX = [
-  'mp4', 'mkv', 'iso',
-]
+try {
+  VIDEO_SUFFIX = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../static/videoSuffix.json'), 'utf-8'))
+} catch (error) {
+}
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import companyFilterFn from './filters/company'
 import targetPath from './utils/targetPath'
 import handler from './handler'
 import action from './action'
+import { COMPANY, VIDEO_SUFFIX } from './constant'
 
-export default async () => {
+const displayBanner = () => {
   const pkgInfo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'))
 
   console.log(`Running ${pkgInfo.name}@v${pkgInfo.version}...`)
+  console.log(`Support company count: ${COMPANY.length}`)
+  console.log(`Support video format file: ${VIDEO_SUFFIX.join(', ')}`)
+}
+
+export default async () => {
+  displayBanner()
   const filenames = fs.readdirSync(targetPath.get())
     .filter(item => !/^~\.@/.test(item))
     .filter(suffixFilterFn)
