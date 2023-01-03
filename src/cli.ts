a4d5afbd84e55f7f60597a6c41b8b2320edc48e6
doubley_yang <doubley_yang@epam.com>
2023-01-04 04:22:20 +0800

    fix: fixed display banner twice when using version option

--- src/cli.ts
+++ src/cli.ts
@@ -4,13 +4,12 @@ import helpProcess from './processes/helpProcess'
 import versionProcess from './processes/versionProcess'
 import { getCliParams } from './utils/getCliParams'
 
-displayBanner()
-
 const params = getCliParams()
 if (params.version) {
   versionProcess()
 } else if (params.help) {
   helpProcess()
 } else {
+  displayBanner()
   main()
 }
