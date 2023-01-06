dbebcdb3e07bc11f83bc29091969f4a61902e081
doubley_yang <doubley_yang@epam.com>
2023-01-06 14:29:19 +0800

    chore: update add company script

--- etc/addCompany.ts
+++ etc/addCompany.ts
@@ -12,14 +12,23 @@ if (basePath) {
   const content: string[] = JSON.parse(fs.readFileSync(filepath, 'utf-8'))
   const oldSize = content.length
   const args = getCliParams()['--']
-  for (const c of args) {
+  args.forEach((c, index) => {
     const exist = content.includes(c.toUpperCase())
     const status = !exist ? chalk.green('PASS') : chalk.blue('SKIP')
-    console.log(`${status} Add Company \`${c}\``)
+    console.log(`${displayNumber(index + 1, args.length)} ${status} Add Company \`${c}\``)
     !exist && content.push(c)
-  }
+  })
   const newContent = [...new Set(content.map(c => c.toUpperCase()))]
   console.log(`Old Size: ${oldSize}`)
   console.log(`New Size: ${newContent.length}`)
   fs.writeFileSync(filepath, JSON.stringify(newContent, null, 2), 'utf-8')
+}
+
+function displayNumber(current: number, total: number) {
+  const { length } = total.toString()
+  let text = current.toString()
+  while (text.length !== length) {
+    text = `0${text}`
+  }
+  return `[${text}/${total}]`
 }
